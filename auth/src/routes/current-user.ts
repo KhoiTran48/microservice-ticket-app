65ade8515d6818eec58ca184949279a9adb074f7
khoi_serdao <dinhkhoi.tran@serdao.fr>
2020-10-22 17:09:50 +0700

    implement for sigin and get logged in user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 
 const currentUserRouter = express.Router()
 
 currentUserRouter.get('/api/users/current-user', (req, res) => {
-    res.send('Hi there!')
+    if(!req.session?.jwt){
+        return res.send({currentUser: null})
+    }
+    
+    try{
+        const payload = jwt.verify(
+            req.session.jwt,
+            process.env.JWT_KEY!
+        )
+        res.send({currentUser: payload})
+    }catch(errr){
+        res.send({currentUser: null})
+    }
 })
 
 export default currentUserRouter; 
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
+import jwt from 'jsonwebtoken';
 import { validateRequest } from './../middlewares/validate-request';
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
+import User from '../models/user'
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
 
 const signinRouter = express.Router()
 
@@ -10,8 +14,27 @@ signinRouter.post('/api/users/signin',
         body('password').trim().notEmpty().withMessage('You must supply a password')
     ],
     validateRequest,
-    (req: Request, res: Response) => {
-        res.send('Hi there!')
+    async (req: Request, res: Response) => {
+        
+        const {email, password} = req.body
+        const existingUser = await User.findOne({email})
+        if(!existingUser){
+            throw new BadRequestError('Invalid credentials')
+        }
+
+        const isPasswordMatch = await Password.compare(existingUser.password, password)
+        if(!isPasswordMatch){
+            throw new BadRequestError('Invalid credentials')
+        }
+        
+        const userJwt = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+        }, process.env.JWT_KEY!)
+
+        req.session = { jwt: userJwt }
+
+        res.status(200).send(existingUser)
     }
 )
 
