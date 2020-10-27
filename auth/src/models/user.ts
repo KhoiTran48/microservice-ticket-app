a5ce110da5f7b7cb9382afcbaf19d01f5fc23366
khoi.tran@email.com <dinhkhoi.tran@serdao.fr>
2020-10-27 11:34:43 +0700

    wrap all page by _app.js

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -54,12 +54,4 @@ userSchema.statics.build = (attrs: UserAttrs) => {
 
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
 
-const user = User.build({
-    email: 'test@test.com',
-    password: "password"
-})
-
-user.password
-user.createdAt
-
 export default User
