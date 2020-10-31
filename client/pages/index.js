b77406edb71a5f1d03e9ab5ffab7b6b656825368
khoi.tran@email.com <dinhkhoi.tran@serdao.fr>
2020-10-31 19:57:40 +0700

    handle where request from for getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,15 @@
-export default () => {
+import buildClient from '../api/build-client'
+
+const LandingPage = ({ currentUser }) => {
+    console.log('I am in the component', currentUser)
     return <h1>Landing Page</h1>
 }
+
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context)
+    const { data } = await client.get('/api/users/current-user')
+    
+    return data 
+}
+
+export default LandingPage
