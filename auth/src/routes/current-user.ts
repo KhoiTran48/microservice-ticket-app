44fd982481db3caf53a52d7684a4ed9b938d6524
khoi.tran@email.com <dinhkhoi.tran@serdao.fr>
2020-11-01 11:24:06 +0700

    add getInitialProps on _app.js

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,10 @@
 import { currentUser } from './../middlewares/current-user';
 import express from 'express'
-import { requireAuth } from '../middlewares/require-auth'
+// import { requireAuth } from '../middlewares/require-auth'
 
 const currentUserRouter = express.Router()
 
-currentUserRouter.get('/api/users/current-user', currentUser, requireAuth, (req, res) => {
+currentUserRouter.get('/api/users/current-user', currentUser, (req, res) => {
     res.send({currentUser: req.currentUser})
 })
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,18 @@
 import buildClient from '../api/build-client'
 
 const LandingPage = ({ currentUser }) => {
-    console.log('I am in the component', currentUser)
-    return <h1>Landing Page</h1>
+    return currentUser ? (
+        <h1>You are signed in</h1>
+    ) : (
+        <h1>You are NOT signed in</h1>
+    )
 }
 
 LandingPage.getInitialProps = async (context) => {
+    console.log('getInitialProps LandingPage')
     const client = buildClient(context)
     const { data } = await client.get('/api/users/current-user')
-    
-    return data 
+    return data
 }
 
 export default LandingPage
