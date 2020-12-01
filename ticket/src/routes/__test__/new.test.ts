2eb1a34bec2e73f071a0b7bb2333fd3f08fbc934
khoi.tran@email.com <dinhkhoi.tran@serdao.fr>
2020-12-02 05:00:51 +0700

    add route for new ticket

--- ticket/src/routes/__test__/new.test.ts
+++ ticket/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import Ticket from '../../models/ticket'
 
 test('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -20,9 +21,64 @@ test('returns a status other than 401 if the user is signed in', async () => {
 });
 
 test('returns an error if an invalid title is provided', async () => {
-
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10
+    }).expect(400)
+  
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10
+    }).expect(400)
 });
 
-test('returns an error if an invalid price is provided', async () => {});
+test('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'dsadasd',
+      price: -10
+    }).expect(400)
+  
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'dsadasd',
+      price: 'ahihi'
+    }).expect(400)
+  
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'dsadasd',
+    }).expect(400)
+});
 
-test('creates a ticket with valid inputs', async () => {});
+test('creates a ticket with valid inputs', async () => {
+  let tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(0)
+
+  let title = 'title'
+  let price = 10
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price
+    }).expect(201)
+
+  tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(1)
+  expect(tickets[0].price).toEqual(price)
+  expect(tickets[0].title).toEqual(title)
+
+});
