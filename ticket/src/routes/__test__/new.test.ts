c4408425109aa713f21bdd5f3f4c661564693b6c
khoi.tran@email.com <dinhkhoi.tran@serdao.fr>
2021-02-12 14:18:58 +0700

    add testing for NATs client

--- ticket/src/routes/__test__/new.test.ts
+++ ticket/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import Ticket from '../../models/ticket'
+import { natsWrapper } from '../../nats-wrapper';
 
 test('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -81,4 +82,19 @@ test('creates a ticket with valid inputs', async () => {
   expect(tickets[0].price).toEqual(price)
   expect(tickets[0].title).toEqual(title)
 
+});
+
+test('publishes an event', async () => {
+
+  let title = 'title'
+  let price = 10
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title,
+      price
+    }).expect(201)
+  
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
 });
--- ticket/src/routes/__test__/update.test.ts
+++ ticket/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest'
 import { app } from '../../app'
 import mongoose from 'mongoose'
+import { natsWrapper } from '../../nats-wrapper'
 
 test('returns a 404 if the provided id does not exist', async () => {
     const id = new mongoose.Types.ObjectId().toHexString()
@@ -93,4 +94,28 @@ test('updates the ticket provided valid inputs', async () => {
     
     expect(ticket.body.title).toEqual(updateTitle)
     expect(ticket.body.price).toEqual(updatePrice)
+})
+
+test('publishes an event', async () => {
+    const cookie = global.signin()
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'Title nek',
+            price: 20
+        })
+    
+    const updateTitle = 'Title update'
+    const updatePrice = 50
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: updateTitle,
+            price: updatePrice
+        })
+        .expect(200)
+    
+    expect(natsWrapper.client.publish).toHaveBeenCalled()
 })
