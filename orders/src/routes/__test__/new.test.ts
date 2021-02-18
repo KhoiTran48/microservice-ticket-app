0f78025b3c1d9a23f4a4639cc53183ad711b16a3
khoi.tran@email.com <dinhkhoi.tran@serdao.fr>
2021-02-18 19:17:57 +0700

    add testing for creating order

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose'
+import request from 'supertest'
+import { app } from '../../app'
+import { Order, OrderStatus } from '../../models/order'
+import { Ticket } from '../../models/ticket'
+
+test('returns an error if the ticket does not exist', async () => {
+    const ticketId = mongoose.Types.ObjectId()
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId })
+        .expect(404)
+})
+
+test('returns an error if the ticket is already reserved', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    })
+    await ticket.save()
+
+    const order = Order.build({
+        ticket,
+        userId: 'randomId',
+        status: OrderStatus.Created,
+        expiresAt: new Date()
+    })
+    await order.save()
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId: ticket.id})
+        .expect(400)
+})
+
+test('reserves a ticket', async () => {
+    let orders = await Order.find({})
+    expect(orders.length).toEqual(0)
+
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    })
+    await ticket.save()
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId: ticket.id})
+        .expect(201)
+    
+    orders = await Order.find({})
+    expect(orders.length).toEqual(1)
+    expect(orders[0].status).toEqual(OrderStatus.Created)
+})
