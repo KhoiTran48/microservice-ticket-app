8e2622d88b74c6b475410f12cf1fc5dac2962ea9
khoi.tran@email.com <dinhkhoi.tran@serdao.fr>
2021-02-20 13:14:09 +0700

    publish cancelled order event on order service

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest'
 import { app } from '../../app'
 import { Ticket } from '../../models/ticket'
 import { Order, OrderStatus } from '../../models/order'
+import { natsWrapper } from '../../nats-wrapper'
 
 test('mark an order as cancelled', async () => {
     const ticket = await Ticket.build({
@@ -26,7 +27,27 @@ test('mark an order as cancelled', async () => {
     const updatedOrder = await Order.findById(order.id)
 
     expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
-
 })
 
-test.todo('emit an cancelled order event')
+test('emit an cancelled order event', async () => {
+    const ticket = await Ticket.build({
+        title: 'concert',
+        price: 20
+    }).save()
+
+    const sessUser = global.signin()
+
+    const { body: order } = await request(app)
+        .post('/api/orders')
+        .set('Cookie', sessUser)
+        .send({ ticketId: ticket.id})
+        .expect(201)
+    
+    await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie', sessUser)
+        .send()
+        .expect(204)
+    
+    expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
