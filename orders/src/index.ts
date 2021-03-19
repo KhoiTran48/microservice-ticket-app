e193aceb8bcf81a8aef04c24d98b16fd8b2331ba
user <user@users-MacBook-Pro.local>
2021-03-19 10:31:17 +0700

    update start message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,7 +7,7 @@ import { TicketUpdatedListener } from './events/listeners/ticket-updated-listene
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
-  console.log('Start order service.')
+  console.log('Start order service..')
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
